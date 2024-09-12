8e63256d9ecc8e09158e6f4dcec083414d3a30f8
Marcellnagy05 <marcika863@gmail.com>
2024-09-12 09:42:59 +0200

    improvement

--- function.js
+++ function.js
@@ -1,8 +1,13 @@
 function getFirstElement (arr){
     //ide írd kódod
-    return arr[0];
+    if (arr.length != 0) {
+        return arr[0];
+    }else{
+        return "Error, no element found!"
+    }
     }
     const numbers = [1, 2, 3];
     const words = ["apple", "banana", "cherry"];
     console.log(`First number: ${getFirstElement(numbers)}`);
     console.log(`First word: ${getFirstElement(words)}`);
+
