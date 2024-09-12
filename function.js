78fa80eed6b0cd7504b93815cef58403a5eaa5dd
Marcellnagy05 <marcika863@gmail.com>
2024-09-12 09:46:00 +0200

    all element

--- function.js
+++ function.js
@@ -1,6 +1,8 @@
 function getFirstElement (arr){
     //ide írd kódod
-    return arr[0];
+    arr.forEach(element => {
+        console.log(element);
+    });
     }
     const numbers = [1, 2, 3];
     const words = ["apple", "banana", "cherry"];
