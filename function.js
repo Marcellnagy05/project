bcd0700c67042b0077e12acbb03d192891bacc31
Marcellnagy05 <marcika863@gmail.com>
2024-09-12 09:40:28 +0200

    First element

---
+++ function.js
@@ -0,0 +1,8 @@
+function getFirstElement (arr){
+    //ide írd kódod
+    return arr[0];
+    }
+    const numbers = [1, 2, 3];
+    const words = ["apple", "banana", "cherry"];
+    console.log(`First number: ${getFirstElement(numbers)}`);
+    console.log(`First word: ${getFirstElement(words)}`);
