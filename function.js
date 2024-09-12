5c5d2abc26468531f5f5bfdd1b8ee51929fabbf6
Marcellnagy05 <marcika863@gmail.com>
2024-09-12 09:45:14 +0200

    util function factorial

--- function.js
+++ function.js
@@ -6,3 +6,9 @@ function getFirstElement (arr){
     const words = ["apple", "banana", "cherry"];
     console.log(`First number: ${getFirstElement(numbers)}`);
     console.log(`First word: ${getFirstElement(words)}`);
+
+    export function factorial(n) {
+        //ide írd a kódod
+        return n**n
+        }
+        console.log(`Factorial of 5: ${factorial(5)}`);
